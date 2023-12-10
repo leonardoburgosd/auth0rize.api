8c4bc91391cdf3afedfdb4a4c908cc9f1c1c99ee
Leonardo Burgos Diaz <lbd_2014@hotmail.com>
2023-12-10 13:17:37 -0500

    Add: get application

--- cens.auth.infraestructure/StoredProcedures/ApplicationProcedure.cs
+++ cens.auth.infraestructure/StoredProcedures/ApplicationProcedure.cs
@@ -3,7 +3,7 @@ namespace cens.auth.infraestructure.StoredProcedures
     public static class ApplicationProcedure
     {
         public static string create = "";
-        public static string get = "";
+        public static string get = "domain.Application_Get";
         public static string update = "";
         public static string delete = "";
     }
