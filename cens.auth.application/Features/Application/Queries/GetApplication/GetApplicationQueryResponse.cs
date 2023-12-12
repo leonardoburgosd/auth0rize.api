7628d5fbf621e0d1ffd13ebaa5b14f1ecca7904a
Leonardo Burgos Diaz <lbd_2014@hotmail.com>
2023-12-12 14:19:33 -0500

    Add: application key

--- cens.auth.application/Features/Application/Queries/GetApplication/GetApplicationQueryResponse.cs
+++ cens.auth.application/Features/Application/Queries/GetApplication/GetApplicationQueryResponse.cs
@@ -4,6 +4,7 @@ namespace cens.auth.application.Features.Application.Queries.GetApplication
     public class GetApplicationQueryResponse
     {
         public int ApplicationId { get; set; }
+        public string Key { get; set; }
         public string Icon { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
